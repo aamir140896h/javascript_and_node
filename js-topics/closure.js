590dc3bac050921e4bbf184ae572d31477c3ecfd
aamir140896h <aamir140896h@gmail.com>
2025-03-09 13:58:31 +0530

    ne changes

--- js-topics/closure.js
+++ js-topics/closure.js
@@ -1,27 +1,27 @@
-function outerFunction(outerVar) {
-  console.log("outer function executed");
+// function outerFunction(outerVar) {
+//   console.log("outer function executed");
 
-  function innerFunction(innerVar) {
-    console.log("outerVar:", outerVar);
-    console.log("innerVar:", innerVar);
-  }
-  innerFunction("inside");
-}
-
-outerFunction("outside");
-
-// function counter() {
-//   let count = 0;
-//   return function () {
-//     count++;
-//     console.log(count);
+//   return function innerFunction(innerVar) {
+//     console.log("outerVar:", outerVar);
+//     console.log("innerVar:", innerVar);
 //   };
 // }
 
-// const increament = counter();
-// increament();
-// increament();
-// increament();
+// const result = outerFunction("hello ");
+// result(" world");
+
+function counter() {
+  let count = 0;
+  return function () {
+    count++;
+    console.log(count);
+  };
+}
+
+const increament = counter();
+increament();
+increament();
+increament();
 
 // var a = 9;
 // var a = 10;
